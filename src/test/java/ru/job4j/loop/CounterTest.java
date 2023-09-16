2b1adcbe1a79457ba7e17e72cacb809a7bec6f7f
eganshin <iEganshin99@gmail.com>
2023-09-16 20:18:28 +0500

    5.1. Подсчет суммы чётных чисел в диапазоне [#192]

---
+++ src/test/java/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,71 @@
+package ru.job4j.loop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CounterTest {
+
+    @Test
+    void testSumFromZeroToTen() {
+        int start = 0;
+        int finish = 10;
+        int result = Counter.sum(start, finish);
+        int expected = 55;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void testSumFromThreeToEight() {
+        int start = 3;
+        int finish = 8;
+        int result = Counter.sum(start, finish);
+        int expected = 33;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void testSumFromOneToOne() {
+        int start = 1;
+        int finish = 1;
+        int result = Counter.sum(start, finish);
+        int expected = 1;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumEvenNumbersFromOneToTenThenThirty() {
+        int start = 1;
+        int finish = 10;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 30;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumEvenNumbersFromZeroToTenThenThirty() {
+        int start = 0;
+        int finish = 10;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 30;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumEvenNumbersFromThreeToEightThenEighteen() {
+        int start = 3;
+        int finish = 8;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 18;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumEvenNumbersFromOneToOneThenZero() {
+        int start = 1;
+        int finish = 1;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 0;
+        assertThat(result).isEqualTo(expected);
+    }
+}
